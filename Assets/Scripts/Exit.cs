50f880b57bab07920e17b72cd4307f05ff82f8f4
SuryanshPrabhakar <suryanshprabhakar@gmail.com>
2017-05-20 13:36:20 +0530

    Add files via upload

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Exit : MonoBehaviour {
+	void Update() {
+		if (Input.GetKey ("escape"))
+			Application.Quit ();
+
+	}
+
+	public Button yourButton;
+
+	void Start()
+	{
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		Application.Quit ();
+		Debug.Log("You have clicked the button!");
+	}
+}
